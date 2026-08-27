5976fe67e49f770e8ff7024ed0df4afdbf140847
Ahmet Shamiev <61404173+ahmet-shamiev@users.noreply.github.com>
2026-08-27 17:45:26 +0600

    Add files via upload

--- splat-survey-v2/netlify/functions/events.js
+++ splat-survey-v2/netlify/functions/events.js
@@ -32,6 +32,7 @@ exports.handler = async function(event, context) {
     notes:       'Notes',
     time:        'Event Time',
     adminNotes:  'Admin Notes',
+    slots:       'Available Slots',
     recordId:    'Record ID',
   };
 
@@ -71,6 +72,7 @@ exports.handler = async function(event, context) {
           notes:   unwrap(f[FIELD_MAP.notes]),
           time:    unwrap(f[FIELD_MAP.time]),
           adminNotes: unwrap(f[FIELD_MAP.adminNotes]),
+          slots:      unwrap(f[FIELD_MAP.slots]),
           recordId:   unwrap(f[FIELD_MAP.recordId]),
         });
       });
